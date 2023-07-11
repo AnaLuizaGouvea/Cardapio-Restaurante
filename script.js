117c6829b397e481fa168e9ad690c8bf10fce4f8
Ana Luiza Gouvêa <analuiza.ogouvea@gmail.com>
2023-07-11 15:29:20 -0300

    media

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+var ul= document.querySelector('.navbar ul')
+var menuBtn = document.querySelector('#burguer')
+
+function MenuShow(){
+    if(ul.classList.contains('open')){
+        ul.classList.remove('open');
+    }else{
+        ul.classList.add('open');
+    }
+}
